c94d57c54ec8277e5112d2cb35cdb9a27adc6f8d
JIYE KIM <hyo37009@gmail.com>
2023-07-31 21:46:38 +0900

    230731 fin ch1 끝!

--- ch9/app/src/main/java/com/example/ch9/MainActivity.kt
+++ ch9/app/src/main/java/com/example/ch9/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.example.ch9
 
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ch9.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    private var mediaPlayer:MediaPlayer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.playButton.setOnClickListener { mediaPlayerPlay() }
+        binding.pauseButton.setOnClickListener { mediaPlayerPause() }
+        binding.stopButton.setOnClickListener { mediaPlayerStop() }
     }
+
+    private fun mediaPlayerPlay(){
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PLAY }
+        startService(intent)
+    }
+
+    private fun mediaPlayerPause(){
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_PAUSE }
+        startService(intent)    }
+
+    private fun mediaPlayerStop(){
+        val intent = Intent(this, MediaPlayerService::class.java)
+            .apply { action = MEDIA_PLAYER_STOP }
+        startService(intent)
+    }
+
+    override fun onDestroy() {
+        stopService(Intent(this, MediaPlayerService::class.java))
+        super.onDestroy()
+    }
+
 }
