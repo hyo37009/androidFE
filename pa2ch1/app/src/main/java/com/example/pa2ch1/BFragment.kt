dba0741341d2a2d691d175408eb4f663db5e4a80
JIYE KIM <hyo37009@gmail.com>
2023-08-04 12:33:36 +0900

    230804

---
+++ pa2ch1/app/src/main/java/com/example/pa2ch1/BFragment.kt
@@ -0,0 +1,6 @@
+package com.example.pa2ch1
+
+import androidx.fragment.app.Fragment
+
+class BFragment:Fragment() {
+}
--- pa2ch1/app/src/main/java/com/example/pa2ch1/MainActivity.kt
+++ pa2ch1/app/src/main/java/com/example/pa2ch1/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.pa2ch1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+
+        webView.loadUrl("https://google.com")
     }
 }
